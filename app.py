213ead61ec07b0693ad511958742eab955010dc4
varshaa476 <varshaprabhudev03@gmail.com>
2026-07-07 11:30:31 +0000

    Add To-Do page with Item Name and Item Description fields (master_1)

--- app.py
+++ app.py
@@ -43,5 +43,9 @@ def submit():
 def success():
     return render_template("success.html")
 
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
